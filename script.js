e4d7c09d49e4aeabfb48b2c7d2129748926a46f8
mannuccimattia <mannuccimattia@gmail.com>
2025-03-17 15:01:47 +0100

    [UPD] Done script underagePrice

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ let ticketPrice = 0.21 * kmNum;
 console.log("Il prezzo del biglietto intero è €", ticketPrice);
 
 // chiedo all'utente di inserire l'età
+let userAge = prompt("Quanti anni hai?");
+
+// SE userAge < 18 anni
+if (userAge < 18){
+  let underagePrice = ticketPrice * 0.8;
+  console.log("Il prezzo del tuo biglietto è €", underagePrice.toFixed(2));
+}
+
