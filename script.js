0c1659f41f82e716995862d69439368e6d78ea3e
mannuccimattia <mannuccimattia@gmail.com>
2025-03-17 15:19:02 +0100

    [FIX] Added parseInt

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // chiedo all'utente di inserire il chilometraggio
-let kmNum = prompt("Quanti chilometri vuoi percorrere?");
+let kmNum = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 
 // calcolo il prezzo del biglietto
 let ticketPrice = 0.21 * kmNum;
 console.log("Il prezzo del biglietto intero è €", ticketPrice);
 
 // chiedo all'utente di inserire l'età
-let userAge = prompt("Quanti anni hai?");
+let userAge = parseInt(prompt("Quanti anni hai?"));
 
 // IF userAge < 18 anni
 if (userAge < 18){
